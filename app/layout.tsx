75f378af57e131fbd08f39cb8dfe5ce3126faaae
Davey Jacobson <daveyjake21@gmail.com>
2024-09-29 02:36:50 -0500

    Initial Commit

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+import { inter } from '@app/ui/fonts';
+import '@app/ui/global.css';
+import React from 'react';
+
+type RootLayoutArgs = {
+  children: React.ReactNode;
+};
+
+export default function RootLayout({ children }: RootLayoutArgs ) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
